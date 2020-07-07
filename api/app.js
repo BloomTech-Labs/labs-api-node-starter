e3b0d64333d62b8c9fd2889313ff8f3d0458f706
Jake Mallory <jake@tinomen.com>
2020-07-07 16:59:57 -0600

    remove user table and seeds

--- api/app.js
+++ api/app.js
@@ -19,13 +19,13 @@ var app = express();
 // app.use('/apidoc', express.static('apidoc'));
 
 app.use(helmet());
+app.use(express.json());
 app.use(
   cors({
     origin: '*',
   })
 );
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
