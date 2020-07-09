1a4f7ab05f0deea6b47780c2d4659ac281b170a7
Jake Mallory <jake@tinomen.com>
2020-07-09 17:30:39 -0600

    add migrations/seeds to production config

--- config/knexfile.js
+++ config/knexfile.js
@@ -23,5 +23,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
   },
 };
