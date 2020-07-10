4bedc3e4a992607f08a51e853e2103de34a47838
Jake Mallory <jake@tinomen.com>
2020-07-10 11:58:15 -0600

    ✅ working swagger doc for root path 🙌🎉🍄

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,24 +6,26 @@ var router = express.Router();
  * /:
  *  get:
  *    description: root path returning status
- *  produces:
- *    - applicaiton/json
- *  responses:
- *    200:
- *      description: status is up
- *      content:
- *        application/xml:
- *          schema:
- *            type: object
- *            required:
- *              - api
- *            properties:
- *              api:
- *                type: boolean
- *                example: true
+ *    tags:
+ *      - status
+ *    produces:
+ *      - applicaiton/json
+ *    responses:
+ *      200:
+ *        description: status is up
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              required:
+ *                - api
+ *              properties:
+ *                api:
+ *                  type: boolean
+ *                  example: true
  */
 router.get('/', function (req, res) {
-  res.status(200).json({ api: 'up' });
+  res.status(200).json({ api: 'up', timestamp: Date.now() });
 });
 
 module.exports = router;
